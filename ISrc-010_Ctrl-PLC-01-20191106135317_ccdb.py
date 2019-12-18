beb1e4a72158da089d85cb2992482b7ac48f3f6c
antonisimelio <antonisimelio@ci0021274.esss.lu.se>
2019-12-18 16:08:19 +0100

    Test in CS Lab

---
+++ ISrc-010_Ctrl-PLC-01-20191106135317_ccdb.py
@@ -0,0 +1,417 @@
+#!/usr/bin/env python2
+
+import os
+import sys
+
+sys.path.append(os.path.curdir)
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
+from ccdb_factory import CCDB_Factory
+
+factory = CCDB_Factory()
+
+# External links for ISRC_LEBT_PS_REP
+factory.addLink("ISRC_LEBT_PS_REP", "EPI", "https://bitbucket.org/europeanspallationsource/isrc-lebt-plc-def")
+
+# External links for ISRC_LEBT_HVPf_Res
+factory.addLink("ISRC_LEBT_HVPf_Res", "EPI", "https://bitbucket.org/europeanspallationsource/isrc-lebt-plc-def")
+
+# External links for ISRC_LEBT_PS_SOL_STR
+factory.addLink("ISRC_LEBT_PS_SOL_STR", "EPI", "https://bitbucket.org/europeanspallationsource/isrc-lebt-plc-def")
+
+# External links for ISRC_LEBT_INTF_PSS
+factory.addLink("ISRC_LEBT_INTF_PSS", "EPI", "https://bitbucket.org/europeanspallationsource/isrc-lebt-plc-def")
+
+# External links for ISRC_LEBT_INTF_DIAG
+factory.addLink("ISRC_LEBT_INTF_DIAG", "EPI", "https://bitbucket.org/europeanspallationsource/isrc-lebt-plc-def")
+
+# External links for ISRC_LEBT_VAC_VVA
+factory.addLink("ISRC_LEBT_VAC_VVA", "EPI", "https://bitbucket.org/europeanspallationsource/isrc-lebt-plc-def")
+
+# External links for ISRC_LEBT_REP
+factory.addLink("ISRC_LEBT_REP", "EPI", "https://bitbucket.org/europeanspallationsource/isrc-lebt-plc-def")
+
+# External links for ISRC_LEBT_STR
+factory.addLink("ISRC_LEBT_STR", "EPI", "https://bitbucket.org/europeanspallationsource/isrc-lebt-plc-def")
+
+# External links for ISRC_LEBT_INTF_EMU
+factory.addLink("ISRC_LEBT_INTF_EMU", "EPI", "https://bitbucket.org/europeanspallationsource/isrc-lebt-plc-def")
+
+# External links for ISRC_LEBT_INTF_DPL
+factory.addLink("ISRC_LEBT_INTF_DPL", "EPI", "https://bitbucket.org/europeanspallationsource/isrc-lebt-plc-def")
+
+# External links for ISRC_LEBT_PBI_EMU
+factory.addLink("ISRC_LEBT_PBI_EMU", "EPI", "https://bitbucket.org/europeanspallationsource/isrc-lebt-plc-def")
+
+# External links for ISRC_LEBT_WTRC_TEMP
+factory.addLink("ISRC_LEBT_WTRC_TEMP", "EPI", "https://bitbucket.org/europeanspallationsource/isrc-lebt-plc-def")
+
+# External links for ISRC_LEBT_HVPf_Doors
+factory.addLink("ISRC_LEBT_HVPf_Doors", "EPI", "https://bitbucket.org/europeanspallationsource/isrc-lebt-plc-def")
+
+# External links for ISRC_LEBT_WTRC_PRES
+factory.addLink("ISRC_LEBT_WTRC_PRES", "EPI", "https://bitbucket.org/europeanspallationsource/isrc-lebt-plc-def")
+
+# External links for ISRC_LEBT_COIL_SOLENOID
+factory.addLink("ISRC_LEBT_COIL_SOLENOID", "EPI", "https://bitbucket.org/europeanspallationsource/isrc-lebt-plc-def")
+
+# External links for ISRC_LEBT_MatTr
+factory.addLink("ISRC_LEBT_MatTr", "EPI", "https://bitbucket.org/europeanspallationsource/isrc-lebt-plc-def")
+
+# External links for ISRC_LEBT_HVIT
+factory.addLink("ISRC_LEBT_HVIT", "EPI", "https://bitbucket.org/europeanspallationsource/isrc-lebt-plc-def")
+
+# External links for ISRC_LEBT_CHOP
+factory.addLink("ISRC_LEBT_CHOP", "EPI", "https://bitbucket.org/europeanspallationsource/isrc-lebt-plc-def")
+
+# External links for ISRC_LEBT_HVPS
+factory.addLink("ISRC_LEBT_HVPS", "EPI", "https://bitbucket.org/europeanspallationsource/isrc-lebt-plc-def")
+
+# External links for ISRC_LEBT_PS_COIL
+factory.addLink("ISRC_LEBT_PS_COIL", "EPI", "https://bitbucket.org/europeanspallationsource/isrc-lebt-plc-def")
+
+# External links for ISRC_LEBT_COLL
+factory.addLink("ISRC_LEBT_COLL", "EPI", "https://bitbucket.org/europeanspallationsource/isrc-lebt-plc-def")
+
+# External links for ISRC_LEBT_INTF_VAC
+factory.addLink("ISRC_LEBT_INTF_VAC", "EPI", "https://bitbucket.org/europeanspallationsource/isrc-lebt-plc-def")
+
+# External links for ISRC_LEBT_BDump
+factory.addLink("ISRC_LEBT_BDump", "EPI", "https://bitbucket.org/europeanspallationsource/isrc-lebt-plc-def")
+
+# External links for ISRC_LEBT_Magtr
+factory.addLink("ISRC_LEBT_Magtr", "EPI", "https://bitbucket.org/europeanspallationsource/isrc-lebt-plc-def")
+
+# External links for ISRC_LEBT_PlasCh
+factory.addLink("ISRC_LEBT_PlasCh", "EPI", "https://bitbucket.org/europeanspallationsource/isrc-lebt-plc-def")
+
+# External links for ISRC_LEBT_IRIS
+factory.addLink("ISRC_LEBT_IRIS", "EPI", "https://bitbucket.org/europeanspallationsource/isrc-lebt-plc-def")
+
+# External links for ISRC_LEBT_PBI_FC
+factory.addLink("ISRC_LEBT_PBI_FC", "EPI", "https://bitbucket.org/europeanspallationsource/isrc-lebt-plc-def")
+
+# External links for ISRC_LEBT_INTF_HMI
+factory.addLink("ISRC_LEBT_INTF_HMI", "EPI", "https://bitbucket.org/europeanspallationsource/isrc-lebt-plc-def")
+
+# External links for ISRC_LEBT_INTF_MPS
+factory.addLink("ISRC_LEBT_INTF_MPS", "EPI", "https://bitbucket.org/europeanspallationsource/isrc-lebt-plc-def")
+
+#
+# Adding PLC: ISrc-010:Ctrl-PLC-01
+#
+plc = factory.addPLC("ISrc-010:Ctrl-PLC-01")
+# Properties
+plc.setProperty("EPICSModule", "['isrc-010_ctrl-plc-01']")
+plc.setProperty("EPICSSnippet", "['isrc-010_ctrl-plc-01.cmd']")
+plc.setProperty("PLCF#EPICSToPLCDataBlockName", "EPICSToPLC")
+plc.setProperty("PLCF#PLCToEPICSDataBlockName", "PLCToEPICS")
+plc.setProperty("PLCF#EPICSToPLCDataBlockStartOffset", "0")
+plc.setProperty("PLCF#PLCToEPICSDataBlockStartOffset", "0")
+plc.setProperty("PLCF#PLC-EPICS-COMMS: MBPort", "502")
+plc.setProperty("PLCF#PLC-EPICS-COMMS: MBConnectionID", "255")
+plc.setProperty("PLCF#PLC-EPICS-COMMS: S7ConnectionID", "256")
+plc.setProperty("PLCF#PLC-EPICS-COMMS: S7Port", "2000")
+plc.setProperty("PLCF#PLC-EPICS-COMMS:Endianness", "BigEndian")
+plc.setProperty("PLCF#PLC-EPICS-COMMS: InterfaceID", "72")
+plc.setProperty("PLCF#PLC-DIAG:Max-IO-Devices", "10")
+plc.setProperty("PLCF#PLC-DIAG:Max-Local-Modules", "10")
+plc.setProperty("PLCF#PLC-DIAG:Max-Modules-In-IO-Device", "15")
+plc.setProperty("Hostname", "null")
+plc.setProperty("PLCF#PLC-EPICS-COMMS: PLCPulse", "Pulse_200ms")
+
+#
+# Adding device LEBT-010:BMD-CH-01 of type ISRC_LEBT_STR
+#
+dev = plc.addDevice("ISRC_LEBT_STR", "LEBT-010:BMD-CH-01")
+
+#
+# Adding device LEBT-010:BMD-CH-02 of type ISRC_LEBT_STR
+#
+dev = plc.addDevice("ISRC_LEBT_STR", "LEBT-010:BMD-CH-02")
+
+#
+# Adding device LEBT-010:BMD-CV-01 of type ISRC_LEBT_STR
+#
+dev = plc.addDevice("ISRC_LEBT_STR", "LEBT-010:BMD-CV-01")
+
+#
+# Adding device LEBT-010:BMD-CV-02 of type ISRC_LEBT_STR
+#
+dev = plc.addDevice("ISRC_LEBT_STR", "LEBT-010:BMD-CV-02")
+
+#
+# Adding device LEBT-010:BMD-Sol-01 of type ISRC_LEBT_COIL_SOLENOID
+#
+dev = plc.addDevice("ISRC_LEBT_COIL_SOLENOID", "LEBT-010:BMD-Sol-01")
+
+#
+# Adding device LEBT-010:BMD-Sol-02 of type ISRC_LEBT_COIL_SOLENOID
+#
+dev = plc.addDevice("ISRC_LEBT_COIL_SOLENOID", "LEBT-010:BMD-Sol-02")
+
+#
+# Adding device LEBT-010:BMD-Chop of type ISRC_LEBT_CHOP
+#
+dev = plc.addDevice("ISRC_LEBT_CHOP", "LEBT-010:BMD-Chop")
+# Properties
+dev.setProperty("EPICSModule", "[]")
+dev.setProperty("EPICSSnippet", "[]")
+
+#
+# Adding device LEBT-010:ID-Coll of type ISRC_LEBT_COLL
+#
+dev = plc.addDevice("ISRC_LEBT_COLL", "LEBT-010:ID-Coll")
+
+#
+# Adding device ISrc-010:WtrC-TT-001 of type ISRC_LEBT_WTRC_TEMP
+#
+dev = plc.addDevice("ISRC_LEBT_WTRC_TEMP", "ISrc-010:WtrC-TT-001")
+
+#
+# Adding device ISrc-010:WtrC-PT-002 of type ISRC_LEBT_WTRC_PRES
+#
+dev = plc.addDevice("ISRC_LEBT_WTRC_PRES", "ISrc-010:WtrC-PT-002")
+
+#
+# Adding device ISrc-010:WtrC-PT-001 of type ISRC_LEBT_WTRC_PRES
+#
+dev = plc.addDevice("ISRC_LEBT_WTRC_PRES", "ISrc-010:WtrC-PT-001")
+
+#
+# Adding device ISrc-010:ISS-Intf-MPS of type ISRC_LEBT_INTF_MPS
+#
+dev = plc.addDevice("ISRC_LEBT_INTF_MPS", "ISrc-010:ISS-Intf-MPS")
+
+#
+# Adding device ISrc-010:ISS-Intf-PSS of type ISRC_LEBT_INTF_PSS
+#
+dev = plc.addDevice("ISRC_LEBT_INTF_PSS", "ISrc-010:ISS-Intf-PSS")
+
+#
+# Adding device LEBT-010:PBI-FC-001 of type ISRC_LEBT_PBI_FC
+#
+dev = plc.addDevice("ISRC_LEBT_PBI_FC", "LEBT-010:PBI-FC-001")
+
+#
+# Adding device ISrc-010:ISS-Intf-HMI of type ISRC_LEBT_INTF_HMI
+#
+dev = plc.addDevice("ISRC_LEBT_INTF_HMI", "ISrc-010:ISS-Intf-HMI")
+
+#
+# Adding device ISrc-010:ISS-HVPS of type ISRC_LEBT_HVPS
+#
+dev = plc.addDevice("ISRC_LEBT_HVPS", "ISrc-010:ISS-HVPS")
+# Properties
+dev.setProperty("EPICSModule", "[]")
+dev.setProperty("EPICSSnippet", "[]")
+
+#
+# Adding device LEBT-010:ID-Iris of type ISRC_LEBT_IRIS
+#
+dev = plc.addDevice("ISRC_LEBT_IRIS", "LEBT-010:ID-Iris")
+# Properties
+dev.setProperty("EPICSModule", "[]")
+dev.setProperty("EPICSSnippet", "[]")
+
+#
+# Adding device LEBT-010:PwrC-PSCV-01 of type ISRC_LEBT_PS_SOL_STR
+#
+dev = plc.addDevice("ISRC_LEBT_PS_SOL_STR", "LEBT-010:PwrC-PSCV-01")
+# Properties
+dev.setProperty("EPICSModule", "[]")
+dev.setProperty("EPICSSnippet", "[]")
+
+#
+# Adding device LEBT-010:PwrC-PSCH-01 of type ISRC_LEBT_PS_SOL_STR
+#
+dev = plc.addDevice("ISRC_LEBT_PS_SOL_STR", "LEBT-010:PwrC-PSCH-01")
+# Properties
+dev.setProperty("EPICSModule", "[]")
+dev.setProperty("EPICSSnippet", "[]")
+
+#
+# Adding device LEBT-010:PwrC-PSCV-02 of type ISRC_LEBT_PS_SOL_STR
+#
+dev = plc.addDevice("ISRC_LEBT_PS_SOL_STR", "LEBT-010:PwrC-PSCV-02")
+# Properties
+dev.setProperty("EPICSModule", "[]")
+dev.setProperty("EPICSSnippet", "[]")
+
+#
+# Adding device LEBT-010:PwrC-PSCH-02 of type ISRC_LEBT_PS_SOL_STR
+#
+dev = plc.addDevice("ISRC_LEBT_PS_SOL_STR", "LEBT-010:PwrC-PSCH-02")
+# Properties
+dev.setProperty("EPICSModule", "[]")
+dev.setProperty("EPICSSnippet", "[]")
+
+#
+# Adding device ISrc-010:PwrC-RepPS-01 of type ISRC_LEBT_PS_REP
+#
+dev = plc.addDevice("ISRC_LEBT_PS_REP", "ISrc-010:PwrC-RepPS-01")
+# Properties
+dev.setProperty("EPICSModule", "[]")
+dev.setProperty("EPICSSnippet", "[]")
+
+#
+# Adding device LEBT-010:PwrC-RepPS-01 of type ISRC_LEBT_PS_REP
+#
+dev = plc.addDevice("ISRC_LEBT_PS_REP", "LEBT-010:PwrC-RepPS-01")
+# Properties
+dev.setProperty("EPICSModule", "[]")
+dev.setProperty("EPICSSnippet", "[]")
+
+#
+# Adding device LEBT-010:PwrC-SolPS-01 of type ISRC_LEBT_PS_SOL_STR
+#
+dev = plc.addDevice("ISRC_LEBT_PS_SOL_STR", "LEBT-010:PwrC-SolPS-01")
+# Properties
+dev.setProperty("EPICSModule", "[]")
+dev.setProperty("EPICSSnippet", "[]")
+
+#
+# Adding device LEBT-010:PwrC-SolPS-02 of type ISRC_LEBT_PS_SOL_STR
+#
+dev = plc.addDevice("ISRC_LEBT_PS_SOL_STR", "LEBT-010:PwrC-SolPS-02")
+# Properties
+dev.setProperty("EPICSModule", "[]")
+dev.setProperty("EPICSSnippet", "[]")
+
+#
+# Adding device ISrc-010:BMD-Rep-01 of type ISRC_LEBT_REP
+#
+dev = plc.addDevice("ISRC_LEBT_REP", "ISrc-010:BMD-Rep-01")
+
+#
+# Adding device ISrc-010:ISS-Intf-Vac of type ISRC_LEBT_INTF_VAC
+#
+dev = plc.addDevice("ISRC_LEBT_INTF_VAC", "ISrc-010:ISS-Intf-Vac")
+
+#
+# Adding device ISrc-010:ISS-MatTr of type ISRC_LEBT_MatTr
+#
+dev = plc.addDevice("ISRC_LEBT_MatTr", "ISrc-010:ISS-MatTr")
+
+#
+# Adding device ISrc-010:ISS-PlasCh of type ISRC_LEBT_PlasCh
+#
+dev = plc.addDevice("ISRC_LEBT_PlasCh", "ISrc-010:ISS-PlasCh")
+
+#
+# Adding device ISrc-010:ISS-Coil-01 of type ISRC_LEBT_COIL_SOLENOID
+#
+dev = plc.addDevice("ISRC_LEBT_COIL_SOLENOID", "ISrc-010:ISS-Coil-01")
+
+#
+# Adding device ISrc-010:ISS-Coil-02 of type ISRC_LEBT_COIL_SOLENOID
+#
+dev = plc.addDevice("ISRC_LEBT_COIL_SOLENOID", "ISrc-010:ISS-Coil-02")
+
+#
+# Adding device ISrc-010:ISS-Coil-03 of type ISRC_LEBT_COIL_SOLENOID
+#
+dev = plc.addDevice("ISRC_LEBT_COIL_SOLENOID", "ISrc-010:ISS-Coil-03")
+
+#
+# Adding device ISrc-010:WtrC-PT-011 of type ISRC_LEBT_WTRC_PRES
+#
+dev = plc.addDevice("ISRC_LEBT_WTRC_PRES", "ISrc-010:WtrC-PT-011")
+
+#
+# Adding device ISrc-010:WtrC-PT-012 of type ISRC_LEBT_WTRC_PRES
+#
+dev = plc.addDevice("ISRC_LEBT_WTRC_PRES", "ISrc-010:WtrC-PT-012")
+
+#
+# Adding device ISrc-010:WtrC-TT-011 of type ISRC_LEBT_WTRC_TEMP
+#
+dev = plc.addDevice("ISRC_LEBT_WTRC_TEMP", "ISrc-010:WtrC-TT-011")
+
+#
+# Adding device ISrc-010:WtrC-TT-012 of type ISRC_LEBT_WTRC_TEMP
+#
+dev = plc.addDevice("ISRC_LEBT_WTRC_TEMP", "ISrc-010:WtrC-TT-012")
+
+#
+# Adding device ISrc-010:Vac-VVA-01100 of type ISRC_LEBT_VAC_VVA
+#
+dev = plc.addDevice("ISRC_LEBT_VAC_VVA", "ISrc-010:Vac-VVA-01100")
+
+#
+# Adding device ISrc-010:ISS-HVPf-Doors of type ISRC_LEBT_HVPf_Doors
+#
+dev = plc.addDevice("ISRC_LEBT_HVPf_Doors", "ISrc-010:ISS-HVPf-Doors")
+
+#
+# Adding device ISrc-010:PwrC-CoilPS-01 of type ISRC_LEBT_PS_COIL
+#
+dev = plc.addDevice("ISRC_LEBT_PS_COIL", "ISrc-010:PwrC-CoilPS-01")
+# Properties
+dev.setProperty("EPICSModule", "[]")
+dev.setProperty("EPICSSnippet", "[]")
+
+#
+# Adding device ISrc-010:PwrC-CoilPS-02 of type ISRC_LEBT_PS_COIL
+#
+dev = plc.addDevice("ISRC_LEBT_PS_COIL", "ISrc-010:PwrC-CoilPS-02")
+# Properties
+dev.setProperty("EPICSModule", "[]")
+dev.setProperty("EPICSSnippet", "[]")
+
+#
+# Adding device ISrc-010:PwrC-CoilPS-03 of type ISRC_LEBT_PS_COIL
+#
+dev = plc.addDevice("ISRC_LEBT_PS_COIL", "ISrc-010:PwrC-CoilPS-03")
+# Properties
+dev.setProperty("EPICSModule", "[]")
+dev.setProperty("EPICSSnippet", "[]")
+
+#
+# Adding device ISrc-010:ISS-Magtr of type ISRC_LEBT_Magtr
+#
+dev = plc.addDevice("ISRC_LEBT_Magtr", "ISrc-010:ISS-Magtr")
+# Properties
+dev.setProperty("EPICSModule", "[]")
+dev.setProperty("EPICSSnippet", "[]")
+
+#
+# Adding device ISrc-010:PwrC-HVIT-01 of type ISRC_LEBT_HVIT
+#
+dev = plc.addDevice("ISRC_LEBT_HVIT", "ISrc-010:PwrC-HVIT-01")
+
+#
+# Adding device LEBT-020:ID-IBS-001 of type ISRC_LEBT_BDump
+#
+dev = plc.addDevice("ISRC_LEBT_BDump", "LEBT-020:ID-IBS-001")
+
+#
+# Adding device ISrc-010:ISS-HVPf-Res of type ISRC_LEBT_HVPf_Res
+#
+dev = plc.addDevice("ISRC_LEBT_HVPf_Res", "ISrc-010:ISS-HVPf-Res")
+
+#
+# Adding device ISrc-010:ISS-Intf-DIAG of type ISRC_LEBT_INTF_DIAG
+#
+dev = plc.addDevice("ISRC_LEBT_INTF_DIAG", "ISrc-010:ISS-Intf-DIAG")
+
+#
+# Adding device LEBT-010:PBI-EMU-001 of type ISRC_LEBT_PBI_EMU
+#
+dev = plc.addDevice("ISRC_LEBT_PBI_EMU", "LEBT-010:PBI-EMU-001")
+
+#
+# Adding device ISrc-010:ISS-Intf-EMU of type ISRC_LEBT_INTF_EMU
+#
+dev = plc.addDevice("ISRC_LEBT_INTF_EMU", "ISrc-010:ISS-Intf-EMU")
+
+#
+# Adding device ISrc-010:ISS-Intf-DPL of type ISRC_LEBT_INTF_DPL
+#
+dev = plc.addDevice("ISRC_LEBT_INTF_DPL", "ISrc-010:ISS-Intf-DPL")
+
+
+#
+# Saving the created CCDB
+#
+factory.save("ISrc-010:Ctrl-PLC-01")
---
+++ run_module_lab.bash
@@ -0,0 +1,1 @@
+iocsh.bash -r isrc-010_ctrl-plc-01,plcfactory -c 'loadIocsh(isrc-010_ctrl-plc-01.cmd.iocsh, "IPADDR=172.30.5.47, RECVTIMEOUT=3000")'
